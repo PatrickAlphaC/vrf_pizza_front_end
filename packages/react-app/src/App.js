3b9e29c1adaec9c652bfccbdb399b270544d35b5
PatrickAlphaC <54278053+PatrickAlphaC@users.noreply.github.com>
2021-03-12 20:41:34 -0500

    wip

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -4,21 +4,10 @@ import { getDefaultProvider } from "@ethersproject/providers"
 import { ethers } from "ethers"
 import Navbar from "./components/navbar/Navbar"
 import "./App.css"
-import { Body, Button, Header, Image, Link } from "./components"
+import { Body, Button, Image, Link } from "./components"
 import logo from "./pizzame.png"
 
 import { addresses, abis } from "@project/contracts"
-
-async function readOnChainData() {
-  // Should replace with the end-user wallet, e.g. Metamask
-  const defaultProvider = getDefaultProvider()
-  // Create an instance of an ethers.js Contract
-  // Read more about ethers.js on https://docs.ethers.io/v5/api/contract/contract/
-  const ceaErc20 = new Contract(addresses.ceaErc20, abis.erc20, defaultProvider)
-  // A pre-defined address that owns some CEAERC20 tokens
-  const tokenBalance = await ceaErc20.balanceOf("0x3f8CB69d9c0ED01923F11c829BaE4D9a4CB6c82C")
-  console.log({ tokenBalance: tokenBalance.toString() })
-}
 
 async function orderPizza() {
   const provider = new ethers.providers.Web3Provider(window.ethereum)
